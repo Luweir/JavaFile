e8b21a905a4b47052b687b771111514366149696
luweir <463047622@qq.com>
2022-04-15 22:44:21 +0800

    111

--- Leetcode/src/Test/Test.java
+++ Leetcode/src/Test/Test.java
@@ -4,41 +4,6 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Test extends Thread {
-    private static Lock lock = new ReentrantLock();
-    private static Condition condition = lock.newCondition();
-    private int id;
-    private static int cnt = 1;
+public class Test{
 
-    Test(int id) {
-        this.id = id;
-    }
-
-    private static void print(int id) {
-
-    }
-
-    @Override
-    public void run() {
-        while (cnt <= 100) {
-            lock.lock();
-            System.out.println("thread_" + id + " num:" + cnt);
-            cnt++;
-            condition.signal();
-            try {
-                if (cnt <= 100)
-                    condition.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            lock.unlock();
-        }
-    }
-
-    public static void main(String[] args) {
-        Thread thread0 = new Test(1);
-        Thread thread1 = new Test(2);
-        thread0.start();
-        thread1.start();
-    }
 }
