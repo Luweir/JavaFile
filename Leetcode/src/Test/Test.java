ed65429f590d921c6cf96e1cdceda1b71ec8e70a
luweir <463047622@qq.com>
2022-04-17 22:20:34 +0800

    12111

--- Leetcode/src/Test/Test.java
+++ Leetcode/src/Test/Test.java
@@ -1,29 +1,46 @@
 package Test;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Test {
+    public String res = "";
+    public int maxCount = 0;
 
-    public static boolean isNumberAndChar(String str) {
-        // 正则匹配式有误
-        boolean flag1 = false;
-        boolean flag2 = false;
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-                flag1 = true;
-            } else if (c >= '0' && c <= '9') {
-                flag2 = true;
-            } else {
-                return false;
-            }
+    public void check(HashMap<String, Integer> stringCount, String s) {
+        int curCount = stringCount.getOrDefault(s, 0);
+        if (curCount + 1 > maxCount) {
+            maxCount = curCount + 1;
+            res = s;
         }
-        return flag1 && flag2;
+        stringCount.put(s, curCount + 1);
+    }
+
+    public String mostCommonWord(String paragraph, String[] banned) {
+        if (paragraph == null || paragraph.length() == 0)
+            return res;
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.addAll(Arrays.asList(banned));
+        HashMap<String, Integer> stringCount = new HashMap<>();
+        int p1 = 0, p2 = 0;
+        while (p2 < paragraph.length()) {
+            char c = paragraph.charAt(p2);
+            if (c == ',' || c == ' ' || c == '!' || c == '?' || c == ':' || c == '.') {
+                String cur = paragraph.substring(p1, p2);
+                cur = cur.toLowerCase();
+                if (!hashSet.contains(cur)) {
+
+                    check(stringCount, cur);
+                }
+                p2 += c == ' ' ? 0 : 1;
+                p1 = p2 + 1;
+            }
+            p2++;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new String[]{"hit"}));
     }
 }
