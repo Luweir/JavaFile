43e586f9f9da67eb6fc3720f96b47aec7f219a8f
luweir <463047622@qq.com>
2022-04-18 13:07:23 +0800

    1111

--- Leetcode/src/Test/Test.java
+++ Leetcode/src/Test/Test.java
@@ -2,50 +2,25 @@ package Test;
 
 import java.util.*;
 
-<<<<<<< HEAD
-public class Test{
-
-=======
 public class Test {
-    public String res = "";
-    public int maxCount = 0;
-
-    public void check(HashMap<String, Integer> stringCount, String s) {
-        int curCount = stringCount.getOrDefault(s, 0);
-        if (curCount + 1 > maxCount) {
-            maxCount = curCount + 1;
-            res = s;
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
         }
-        stringCount.put(s, curCount + 1);
+        list.sort((t1, t2) ->
+                t1.toString().compareTo(t2.toString())
+        );
+        return list;
     }
 
-    public String mostCommonWord(String paragraph, String[] banned) {
-        if (paragraph == null || paragraph.length() == 0)
-            return res;
-        HashSet<String> hashSet = new HashSet<>();
-        hashSet.addAll(Arrays.asList(banned));
-        HashMap<String, Integer> stringCount = new HashMap<>();
-        int p1 = 0, p2 = 0;
-        while (p2 < paragraph.length()) {
-            char c = paragraph.charAt(p2);
-            if (c == ',' || c == ' ' || c == '!' || c == '?' || c == ':' || c == '.') {
-                String cur = paragraph.substring(p1, p2);
-                cur = cur.toLowerCase();
-                if (!hashSet.contains(cur)) {
 
-                    check(stringCount, cur);
-                }
-                p2 += c == ' ' ? 0 : 1;
-                p1 = p2 + 1;
-            }
-            p2++;
-        }
-        return res;
-    }
 
     public static void main(String[] args) {
         Test test = new Test();
-        System.out.println(test.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new String[]{"hit"}));
+        List<Integer> res = test.lexicalOrder(13);
+        for (int i = 0; i < res.size(); i++) {
+            System.out.println(res.get(i));
+        }
     }
->>>>>>> ed65429f590d921c6cf96e1cdceda1b71ec8e70a
 }
