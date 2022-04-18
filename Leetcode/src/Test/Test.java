d5ba8c372661945ed4dcecc0a38a673683d70637
luweir <463047622@qq.com>
2022-04-19 00:26:58 +0800

    12111

--- Leetcode/src/Test/Test.java
+++ Leetcode/src/Test/Test.java
@@ -3,24 +3,11 @@ package Test;
 import java.util.*;
 
 public class Test {
-    public List<Integer> lexicalOrder(int n) {
-        List<Integer> list = new LinkedList<>();
-        for (int i = 1; i <= n; i++) {
-            list.add(i);
-        }
-        list.sort((t1, t2) ->
-                t1.toString().compareTo(t2.toString())
-        );
-        return list;
-    }
-
 
 
     public static void main(String[] args) {
-        Test test = new Test();
-        List<Integer> res = test.lexicalOrder(13);
-        for (int i = 0; i < res.size(); i++) {
-            System.out.println(res.get(i));
-        }
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(n * (n - 1) - 1);
     }
 }
