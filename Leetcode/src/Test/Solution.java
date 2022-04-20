312f9053089bc43c59486bd4d20300abbfd83156
luweir <463047622@qq.com>
2022-04-20 11:00:50 +0800

    1111

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -1,51 +1,59 @@
 package Test;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 public class Solution {
-    public void recoverTree(TreeNode root) {
-        if (root == null)
-            return;
-        Stack<TreeNode> stack = new Stack<>();
-        List<TreeNode> nodeList = new LinkedList<>();
-        // 得到中序遍历
-        while (!stack.isEmpty() || root != null) {
-            if (root != null ) {
-                stack.add(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            nodeList.add(root);
-            root = root.right;
+    public int res = 0;
+
+    public boolean isFile(String s) {
+        for (int i = s.length() - 1; i >= 1; i--) {
+            if (s.charAt(i) == '.')
+                return true;
         }
-        int p1 = 0, p2 = nodeList.size() - 1;
-        while (p1 < p2 && nodeList.get(p1).val <= nodeList.get(p1 + 1).val)
-            p1++;
-        while (p1 < p2 && nodeList.get(p2).val >= nodeList.get(p2 - 1).val)
-            p2--;
-        int temp = nodeList.get(p1).val;
-        nodeList.get(p1).val = nodeList.get(p2).val;
-        nodeList.get(p2).val = temp;
+        return false;
+    }
+
+    public void process(String[] inputs, int s, int e, int layer, int len) {
+        if (s > e)
+            return;
+        for (int i = s; i <= e; i++) {
+            if (isFile(inputs[i]))
+                res = Math.max(res, len + inputs[i].length() - layer + 1);
+            else {
+                int j = i + 1;
+                for (; j <= e + 1; j++) {
+                    if (j == e + 1 || inputs[j].lastIndexOf('\t') + 1 == layer) {
+                        process(inputs, i + 1, j - 1, layer + 1, len + inputs[i].length() - layer + (layer == 0 ? 0 : 1));
+                        break;
+                    }
+                }
+                i = j - 1;
+            }
+        }
+    }
+
+    public int lengthLongestPath(String input) {
+        if (input == null || input.length() == 0)
+            return 0;
+        String[] inputs = input.split("\n");
+        int n = inputs.length;
+        // 第一个是文件 或者全程没有子目录  那么文件和第一级目录都在第一层
+        if (isFile(inputs[0]) || input.lastIndexOf('\t') == -1) {
+            for (int i = 0; i < n; i++) {
+                if (isFile(inputs[i]))
+                    res = Math.max(inputs[i].length(), res);
+            }
+        } else {
+            process(inputs, 0, n - 1, 0, 0);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthLongestPath("a\n\tb\n\t\tc.txt\n\taaaa.txt"));
     }
 }
