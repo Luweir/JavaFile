a44fb226ca4330a7c42b287f7a5f91a75f4e08a9
luweir <463047622@qq.com>
2022-04-26 22:32:49 +0800

    12111

--- Leetcode/src/Test/Main.java
+++ Leetcode/src/Test/Main.java
@@ -5,11 +5,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Integer x = 123;
-        Integer y = 123;
-        System.out.println(x == y);    // true
-        Integer z = 129;
-        Integer k = 129;
-        System.out.println(z == k);   // false
+
     }
 }
