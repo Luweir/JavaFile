046567c83f9d3148ef242f196a1efc3361805ede
luweir <463047622@qq.com>
2022-04-27 02:14:38 +0800

    12111

--- Leetcode/src/Test/Main.java
+++ Leetcode/src/Test/Main.java
@@ -1,10 +1,13 @@
 package Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 public class Main {
 
     public static void main(String[] args) {
-
     }
 }
