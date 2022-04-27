d089e023737bb738938c1763332c0c2260185a58
luweir <463047622@qq.com>
2022-04-27 09:02:42 +0800

    12111

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -1,39 +1,60 @@
 package Test;
 
+import JUC.PC.A;
 import Link.Link;
 
 import java.util.*;
 
 public class Solution {
-    public static long res = 0;
+    public int[][] position = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    // rest 还有的钱   keep 持股数
-    public static long process(HashMap<String, Long> hashMap, int[] a, int cur, long rest, long keep) {
-        if (cur == a.length - 1) {
-            return rest + keep * a[cur];
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        Queue<Integer> goList = new LinkedList<>();
+
+        List<List<Integer>> res = new LinkedList<>();
+        int m = heights.length;
+        int n = heights[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int mod = Math.max(m, n);
+        goList.add((m - 1) * mod);
+        goList.add(n - 1);
+
+        visited[m - 1][0] = true;
+        visited[0][n - 1] = true;
+        List<Integer> temp = new ArrayList<>(2);
+        temp.add(m - 1);
+        temp.add(0);
+        res.add(temp);
+        temp = new ArrayList<>(2);
+        temp.add(0);
+        temp.add(n - 1);
+        res.add(temp);
+        while (!goList.isEmpty()) {
+            int size = goList.size();
+            while (size-- > 0) {
+                int cur = goList.poll();
+                int x = cur / mod;
+                int y = cur % mod;
+                for (int i = 0; i < 4; i++) {
+                    int newX = x + position[i][0];
+                    int newY = y + position[i][1];
+                    if (newX >= 0 && newX < m && newY >= 0 && newY < n && heights[newX][newY] >= heights[x][y] && !visited[newX][newY]) {
+                        visited[newX][newY] = true;
+                        goList.add(newX * mod + newY);
+                        temp = new ArrayList<>(2);
+                        temp.add(newX);
+                        temp.add(newY);
+                        res.add(temp);
+                    }
+                }
+            }
         }
-        String s = cur + "_" + rest + "_" + keep;
-        if (hashMap.containsKey(s))
-            return hashMap.get(s);
-        long res = -1;
-        if (rest - a[cur] >= 0) {
-            res = Math.max(process(hashMap, a, cur + 1, rest - a[cur], keep + 1), res);
-        }
-        res = Math.max(process(hashMap, a, cur + 1, rest + a[cur], keep - 1), res);
-        hashMap.put(s, res);
         return res;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
-        }
-        HashMap<String, Long> hashMap = new HashMap<>();
-        process(hashMap, a, 0, m, 0);
-        System.out.println(res);
+        int[][] test = {{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
+        Solution main = new Solution();
+        System.out.println(main.pacificAtlantic(test));
     }
 }
