9227f8d1f3bf4868d4660ee5114989f8d0ae5726
luweir <463047622@qq.com>
2022-05-04 10:57:12 +0800

    1111

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -1,49 +1,31 @@
 package Test;
 
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
 
 public class Solution {
-    public String[] reorderLogFiles(String[] logs) {
-        if (logs == null || logs.length <= 1)
-            return logs;
-        PriorityQueue<String> priorityQueue = new PriorityQueue<String>(new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                int p1 = 0, p2 = 0;
-                while (s1.charAt(p1) != ' ')
-                    p1++;
-                while (s2.charAt(p2) != ' ')
-                    p2++;
-                p1++;
-                p2++;
-                int flag = s1.substring(p1).compareTo(s2.substring(p2));
-                return flag == 0 ? s1.substring(0, p1).compareTo(s2.substring(0, p2)) : flag;
-            }
-        });
-        int index = logs.length - 1;
-        for (int i = logs.length - 1; i >= 0; i--) {
-            int p1 = 0;
-            while (logs[i].charAt(p1) != ' ')
-                p1++;
-            p1++;
-            char c = logs[i].charAt(p1);
-            if (c >= 'a' && c <= 'z') {
-                priorityQueue.add(logs[i]);
-            } else {
-                logs[index--] = logs[i];
-            }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+        TreeNode leftGet = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightGet = lowestCommonAncestor(root.right, p, q);
+        if (leftGet != null && rightGet != null) {
+            return root;
         }
-        for (int i = 0; i < logs.length && !priorityQueue.isEmpty(); i++) {
-            logs[i] = priorityQueue.poll();
-        }
-        return logs;
+        return leftGet == null ? rightGet : leftGet;
     }
 
     public static void main(String[] args) {
-        String[] s = {"dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"};
-        System.out.println(Arrays.toString(new Solution().reorderLogFiles(s)));
+
     }
 }
