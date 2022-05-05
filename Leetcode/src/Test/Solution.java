2e63dfdf995854e19a6c1effd2483440dce4db0b
luweir <463047622@qq.com>
2022-05-05 13:37:14 +0800

    1111

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -1,16 +1,39 @@
 package Test;
 
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
-import java.sql.Time;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
+class Pair<T> {
+    private T value;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
+
+class DateInter extends Pair<Date> {
+
+    @Override
+    public void setValue(Date value) {
+        super.setValue(value);
+    }
+
+    @Override
+    public Date getValue() {
+        return super.getValue();
+    }
+}
 
 public class Solution {
+    public static void main(String[] args) {
+        DateInter dateInter = new DateInter();
+        dateInter.setValue(new Date());
+        dateInter.setValue(new Object()); //编译错误
+    }
 }
 
 
