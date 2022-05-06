35533148c47e5c5a18bd407f593287102471beec
luweir <463047622@qq.com>
2022-05-06 10:30:38 +0800

    1111

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -3,36 +3,9 @@ package Test;
 
 import java.util.*;
 
-class Pair<T> {
-    private T value;
-
-    public T getValue() {
-        return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
-    }
-}
-
-class DateInter extends Pair<Date> {
-
-    @Override
-    public void setValue(Date value) {
-        super.setValue(value);
-    }
-
-    @Override
-    public Date getValue() {
-        return super.getValue();
-    }
-}
 
 public class Solution {
     public static void main(String[] args) {
-        DateInter dateInter = new DateInter();
-        dateInter.setValue(new Date());
-        dateInter.setValue(new Object()); //编译错误
     }
 }
 
