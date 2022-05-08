ff31e1772f0ae5712bc1379ec9a357c0965fec88
luweir <463047622@qq.com>
2022-05-08 21:54:01 +0800

    111

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -1,80 +1,46 @@
 package Test;
 
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
-import java.sql.Time;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Solution {
-<<<<<<< HEAD
-=======
-    public static void main(String[] args) {
-        String lockA = "lockA";
-        String lockB = "lockB";
-        new Thread(new MyThread(lockA, lockB)).start();
-        new Thread(new MyThread(lockB, lockA)).start();
-        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
-        Thread checkDeadLock = new Thread(() -> {
-            long[] deadlockedThreads = threadMXBean.findDeadlockedThreads(); // 返回出现死锁的线程ID
-            if (deadlockedThreads != null) {
-                ThreadInfo[] threadInfo = threadMXBean.getThreadInfo(deadlockedThreads); // 获得死锁的线程信息
-                System.out.println("检测到的死锁信息");
-                for (int i = 0; i < threadInfo.length; i++) {
-                    System.out.println(threadInfo[i].getThreadId() + "-----" + threadInfo[i].getThreadName());
-                }
+
+//        // 暴力法：hashMap  o(N) o(N)
+//    public List<Integer> findDuplicates(int[] nums) {
+//        List<Integer> res = new LinkedList<>();
+//        HashSet<Integer> hashSet = new HashSet<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if (hashSet.contains(nums[i]))
+//                res.add(nums[i]);
+//            else
+//                hashSet.add(nums[i]);
+//        }
+//        return res;
+//    }
+    // 利用元素 与 下标的转换
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new LinkedList<>();
+        if (nums.length < 2)
+            return res;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == -1 || nums[i] == 0)
+                continue;
+            int next = nums[i];
+            nums[i] = 0;
+            while (nums[next - 1] != -1 && nums[next - 1] != 0) {
+                int temp = nums[next - 1];
+                nums[next - 1] = -1;
+                next = temp;
             }
-        });
-        // 创建定时任务线程池
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-        service.scheduleAtFixedRate(checkDeadLock, 5, 5, TimeUnit.SECONDS); // checkDeadLock每5s监测一次 监测5次
-    }
-
-}
-
-class MyThread implements Runnable {
-    private String lockGet;
-    private String lockWant;
-
-    public MyThread(String lockGet, String lockWant) {
-        this.lockGet = lockGet;
-        this.lockWant = lockWant;
-    }
-
-    @Override
-    public void run() {
-        synchronized (lockGet) {
-            System.out.println(Thread.currentThread().getName() + "get " + lockGet + ", want " + lockWant);
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            synchronized (lockWant) {
-                System.out.println(Thread.currentThread().getName() + "get " + lockWant + ", want " + lockGet);
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            if (nums[next - 1] == -1)
+                res.add(next);
+            else
+                nums[next - 1] = -1;
         }
+        return res;
     }
->>>>>>> 1fe7acf9d565ded62a439bee5c1e0787b64d2337
-}
-/**
- * Thread-0get lockA, want lockB
- * Thread-1get lockB, want lockA
- * 检测到的死锁信息
- * 14-----Thread-0
- * 15-----Thread-1
- * 检测到的死锁信息
- * 14-----Thread-0
- * 15-----Thread-1
- */
 
+    public static void main(String[] args) {
+        System.out.println(new Solution().findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
+    }
+}
