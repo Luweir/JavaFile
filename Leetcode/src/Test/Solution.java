0d1162d262ec6cdb9a8aec4c8c7c9557cdd20b69
luweir <463047622@qq.com>
2022-05-17 12:06:01 +0800

    111

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -4,8 +4,19 @@ package Test;
 import java.util.*;
 
 public class Solution {
+    static class Inner {
+        public int[] a = new int[100];
+
+        public void print() {
+            System.out.println(a.length);
+        }
+    }
 
     public static void main(String[] args) {
+        Solution solution = new Solution();
+        Inner inner1 = new Inner();
+        Inner inner2 = new Inner();
+        System.out.println(inner1.a == inner2.a);
     }
 
 }
