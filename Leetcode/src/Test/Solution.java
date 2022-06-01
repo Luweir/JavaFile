534e922ba32ba3802aa3202de51781e242a2fe5f
luweir <463047622@qq.com>
2022-06-01 15:19:58 +0800

    123123

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -4,52 +4,34 @@ package Test;
 import java.util.*;
 
 public class Solution {
-    public String validIPAddress(String queryIP) {
-        if (queryIP == null || queryIP.length() == 0)
-            return "Neither";
-        if (queryIP.charAt(queryIP.length() - 1) == '.' || queryIP.charAt(queryIP.length() - 1) == ':')
-            return "Neither";
-        String[] strs = queryIP.split("\\.");
-        if (strs.length == 4) {
-            for (int i = 0; i < 4; i++) {
-                String cur = strs[i];
-                if (cur.length() == 0 || (cur.length() > 1 && cur.charAt(0) == '0'))
-                    return "Neither";
-
-                for (int j = 0; j < cur.length(); j++) {
-                    if (cur.charAt(j) < '0' || cur.charAt(j) > '9')
-                        return "Neither";
-                }
-
-                int num = Integer.parseInt(cur);
-                if (num > 255)
-                    return "Neither";
-            }
-            return "IPv4";
-        } else {
-            strs = queryIP.split(":");
-            if (strs.length != 8)
-                return "Neither";
-            for (int i = 0; i < 8; i++) {
-                String cur = strs[i];
-                cur = cur.toLowerCase();
-                for (int j = 0; j < cur.length(); j++) {
-                    if (cur.length() == 0 || cur.length() > 4)
-                        return "Neither";
-                    if ((cur.charAt(j) <= 'f' && cur.charAt(j) >= 'a') || (cur.charAt(j) <= '9' && cur.charAt(j) >= '0'))
-                        continue;
-                    else {
-                        return "Neither";
-                    }
-                }
-            }
-            return "IPv6";
+    public boolean makesquare(int[] matchsticks) {
+        if (matchsticks.length < 4)
+            return false;
+        int sum = 0;
+        for (int i = 0; i < matchsticks.length; i++) {
+            sum += matchsticks[i];
         }
+        if (sum % 4 != 0)
+            return false;
+        return dfs(0, matchsticks, sum / 4, sum / 4, sum / 4, sum / 4);
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().validIPAddress("2001:db8:85a3:0::8a2E:0370:7334"));
+    private boolean dfs(int i, int[] matchsticks, int i1, int i2, int i3, int i4) {
+        if (i == matchsticks.length)
+            return true;
+        if (matchsticks[i] <= i1 && dfs(i + 1, matchsticks, i1 - matchsticks[i], i2, i3, i4))
+            return true;
+        if (matchsticks[i] <= i2 && dfs(i + 1, matchsticks, i1, i2 - matchsticks[i], i3, i4))
+            return true;
+        if (matchsticks[i] <= i3 && dfs(i + 1, matchsticks, i1, i2, i3 - matchsticks[i], i4))
+            return true;
+        if (matchsticks[i] <= i4 && dfs(i + 1, matchsticks, i1, i2, i3, i4 - matchsticks[i]))
+            return true;
+        return false;
+    }
 
+
+    public static void main(String[] args) {
     }
 }
 
