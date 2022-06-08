1496c70e0d57e8eca8028a71a6092cb5139b1214
luweir <463047622@qq.com>
2022-06-09 01:30:24 +0800

    123

--- Leetcode/src/Test/Solution.java
+++ Leetcode/src/Test/Solution.java
@@ -4,34 +4,10 @@ package Test;
 import java.util.*;
 
 public class Solution {
-    public boolean makesquare(int[] matchsticks) {
-        if (matchsticks.length < 4)
-            return false;
-        int sum = 0;
-        for (int i = 0; i < matchsticks.length; i++) {
-            sum += matchsticks[i];
-        }
-        if (sum % 4 != 0)
-            return false;
-        return dfs(0, matchsticks, sum / 4, sum / 4, sum / 4, sum / 4);
-    }
-
-    private boolean dfs(int i, int[] matchsticks, int i1, int i2, int i3, int i4) {
-        if (i == matchsticks.length)
-            return true;
-        if (matchsticks[i] <= i1 && dfs(i + 1, matchsticks, i1 - matchsticks[i], i2, i3, i4))
-            return true;
-        if (matchsticks[i] <= i2 && dfs(i + 1, matchsticks, i1, i2 - matchsticks[i], i3, i4))
-            return true;
-        if (matchsticks[i] <= i3 && dfs(i + 1, matchsticks, i1, i2, i3 - matchsticks[i], i4))
-            return true;
-        if (matchsticks[i] <= i4 && dfs(i + 1, matchsticks, i1, i2, i3, i4 - matchsticks[i]))
-            return true;
-        return false;
-    }
 
 
     public static void main(String[] args) {
+
     }
 }
 
--- SpringStudy/SpringMVC-demo01/src/main/java/com/atguigu/mvc/controller/HelloController.java
+++ SpringStudy/SpringMVC-demo01/src/main/java/com/atguigu/mvc/controller/HelloController.java
@@ -12,4 +12,9 @@ public class HelloController {
         // 返回视图名称  视图名称决定了我们要跳转的页面  返回后被视图解析器thymeleaf进行解析  注意那里的前缀后缀（去掉前缀去掉后缀）
         return "index";
     }
+
+    @RequestMapping("/target")
+    public String toTarget() {
+        return "target";
+    }
 }
